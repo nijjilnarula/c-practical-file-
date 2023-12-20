8e23cc1feae7e061fe2db3ad4418401a75bf23bd
nijjilnarula <narulanijjil@gmail.com>
2023-12-21 02:16:32 +0530

    g

--- .vscode/practical35/ptractical35.c
+++ .vscode/practical35/ptractical35.c
@@ -1,4 +1,20 @@
-#include<stdio.h>
-int main(){
-    
+#include <stdio.h>
+int factorial(int *n);
+int main()
+{
+    int c;
+    printf("enter the no for the factorial\n");
+    scanf("%d", &c);
+    factorial(&c);
+    printf("the factorial is %d", c);
+}
+int factorial(int *n)
+{
+    int i, fac;
+    fac = 1;
+    for (i = *n; i >= 1; i--)
+    {
+        fac *= i;
+    }
+    *n = fac;
 }
